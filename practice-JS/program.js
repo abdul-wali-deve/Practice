b1b0b4d169092c365b243cce7bc243f43f2dff21
abdul-wali-deve <abdulwaliqasim3@gmail.com>
2025-10-18 18:40:49 +0500

    Practice

---
+++ practice-JS/program.js
@@ -0,0 +1,12 @@
+//operators
+ //Arithematic operators (+ - * / %)
+ var x=10;
+ var y=20;
+ result = x + y;
+ console.log(result);
+
+//comperison operator (==   >   <   >=   <=)
+var x=10;
+var y=20;
+console.log(x>y)
+
