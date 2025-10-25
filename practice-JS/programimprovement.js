df6397c89c66cfaedc5c66f642e643d73c34d8dd
abdul-wali-deve <abdulwaliqasim3@gmail.com>
2025-10-25 19:11:25 +0500

    practice

---
+++ practice-JS/programimprovement.js
@@ -0,0 +1,13 @@
+// 1.Write a JS program to store the name of a user and his age, then display them on the console.
+var userName="Abdulwali"
+var age=19
+console.log("My name is" ,userName, "and my age is " , age)
+
+
+
+// 2.Write a JS program to store the name, roll number, and grade of a student, and print them.
+
+var studentName=prompt("Enter your name")
+var rollNumber=parseInt(prompt("Enter your age"))
+var grade=parseInt(prompt("Entet your grade"))
+console.log("My name is " , studentName , "and my Rollno is " , rollNumber , "and my grade is " ,grade)
